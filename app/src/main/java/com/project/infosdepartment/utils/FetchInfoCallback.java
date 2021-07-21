f055c4c90838db8173cf8975d0693608d063fadf
mexane.delcroix <mexane.delcroix@epita.fr>
2021-07-21 11:46:54 +0200

    feat[FetchInfo] : Get the name of the department from the request

--- app/src/main/java/com/project/infosdepartment/utils/FetchInfoCallback.java
+++ app/src/main/java/com/project/infosdepartment/utils/FetchInfoCallback.java
@@ -9,19 +9,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public interface FetchInfoCallback {
+public class FetchInfoCallback {
 
-    default void onSuccess(JSONArray result, DepartmentsDao departmentsDao, String departmentName, String departmentCode) {
-
+    public void onSuccess(JSONArray result, DepartmentsDao departmentsDao, String departmentCode) {
+        String departmentName = "";
         int nbTowns = result.length();
         int inhabitants = 0;
         for (int i = 0; i < nbTowns; i++) {
             try {
                 JSONObject currentObject = result.getJSONObject(i);
+                if (i == 0) {
+                    JSONObject departmentInfo = currentObject.getJSONObject("departement");
+                    departmentName = departmentInfo.getString("nom");
+                }
                 inhabitants += currentObject.getInt("population");
             } catch (JSONException e) {
 
-                Log.e("[ERROR][FetchInfoCallback]", "onSuccess: An error occurred when converting an element of a JsonArray in JsonObject.");
+                Log.e("[ERROR][FetchInfoCallback]", "onSuccess: An error occurred when converting an element from a JsonArray to a JsonObject.");
             }
 
         }
