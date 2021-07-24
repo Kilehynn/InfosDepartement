e2ca49cdd76706d2116f79bf18920928b198ec44
mexane.delcroix <mexane.delcroix@epita.fr>
2021-07-24 20:45:18 +0200

    UI[Activity] :  start UI

---
+++ app/src/main/java/com/project/infosdepartment/viewmodel/InfoViewModel.java
@@ -0,0 +1,8 @@
+package com.project.infosdepartment.viewmodel;
+
+public class InfoViewModel {
+    String departmentName;
+    String departmentCode;
+    Integer departmentInhabitant;
+    Integer departmentTowns;
+}
---
+++ app/src/main/java/com/project/infosdepartment/views/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.project.infosdepartment.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.infosdepartment.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
