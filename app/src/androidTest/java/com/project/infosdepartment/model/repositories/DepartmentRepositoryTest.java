e63c68806203696e48c0041ad8d86e73b5d16f05
mexane.delcroix <mexane.delcroix@epita.fr>
2021-07-25 20:06:51 +0200

    Test[DepartmentRepository] : GetDepartmentList test

--- app/src/androidTest/java/com/project/infosdepartment/model/repositories/DepartmentRepositoryTest.java
+++ app/src/androidTest/java/com/project/infosdepartment/model/repositories/DepartmentRepositoryTest.java
@@ -4,10 +4,15 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import com.project.infosdepartment.model.database.entity.DepartmentsListEntity;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -21,6 +26,11 @@ public class DepartmentRepositoryTest {
         departmentRepository = new DepartmentRepository(ApplicationProvider.getApplicationContext());
     }
 
+    @After
+    public void clean() {
+        departmentRepository.resetCache();
+    }
+
     @Test
     public void testGetters() {
         Assert.assertEquals("https://geo.api.gouv.fr", DepartmentRepository.getUrlPrefix());
@@ -32,7 +42,7 @@ public class DepartmentRepositoryTest {
     }
 
     @Test
-    public void testFetchInfo() {
+    public void testGetDepartmentInfo() {
         String code = "78";
         try {
             departmentRepository.getDepartmentInfo(code);
@@ -50,16 +60,43 @@ public class DepartmentRepositoryTest {
 
     }
 
-    @Test
-    public void testGetDepartmentInfo() {
-    }
 
     @Test
     public void testGetDepartmentsList() {
+        List<DepartmentsListEntity> list = departmentRepository.getDepartmentsList();
+        Assert.assertEquals(101, list.size());
+        DepartmentsListEntity previous = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            DepartmentsListEntity current = list.get(i);
+            if (current.getDepartmentCode().equals("2A") || current.getDepartmentCode().equals("2B") || previous.getDepartmentCode().equals("2B")) {
+                continue;
+            }
+            int currentCode = Integer.parseInt(current.getDepartmentCode());
+            int previousCode = Integer.parseInt(previous.getDepartmentCode());
+            if (currentCode < previousCode) {
+                Assert.fail();
+            }
+            previous = current;
+        }
     }
 
     @Test
     public void testGetDepartment() {
+        String code = "78";
+        Integer zero = 0;
+        try {
+            DepartmentsListEntity departmentsListEntity = departmentRepository.getDepartment(code);
+            Assert.assertEquals(code, departmentsListEntity.getDepartmentCode());
+            Assert.assertEquals("Yvelines", departmentsListEntity.getDepartmentName());
+            Assert.assertEquals(zero, departmentsListEntity.getAreDataFetched());
+        } catch (RuntimeException e) {
+            Assert.fail();
+        }
+        code = "456";
+        try {
+            departmentRepository.getDepartmentInfo(code);
+        } catch (RuntimeException ignored) {
+        }
     }
 
 }
