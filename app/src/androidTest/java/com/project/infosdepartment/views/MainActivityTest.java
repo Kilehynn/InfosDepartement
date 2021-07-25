16b1e3d289aaffa85174217a48c200c9f7ec54ef
mexane.delcroix <mexane.delcroix@epita.fr>
2021-07-25 13:32:49 +0200

    Test[MainActivityTest] : Add file for instrumented tests

---
+++ app/src/androidTest/java/com/project/infosdepartment/views/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.project.infosdepartment.views;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testOnCreateOptionsMenu() {
+    }
+
+    public void testOnOptionsItemSelected() {
+    }
+}
