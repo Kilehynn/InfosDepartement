2d4f52d95b3c36b7695f059be7aa18da0005496e
mexane.delcroix <mexane.delcroix@epita.fr>
2021-07-25 16:58:50 +0200

    Test [InfoActivity] : Add instrumented test

---
+++ app/src/androidTest/java/com/project/infosdepartment/views/InfoActivityTestSuite.java
@@ -0,0 +1,12 @@
+package com.project.infosdepartment.views;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({InfoActivityDOMTOMTest.class,
+        InfoActivityMetropoleTest.class})
+public class InfoActivityTestSuite {
+}
+
